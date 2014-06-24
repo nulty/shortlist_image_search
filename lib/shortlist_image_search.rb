c41474f6c20a28e89482e3d2a9b6a9a9a6b2d4b4
iain <iain@picturk.com>
2014-06-24 11:15:45 +0100

    Add searcher

--- lib/shortlist_image_search.rb
+++ lib/shortlist_image_search.rb
@@ -1,6 +1,48 @@
 require "shortlist_image_search/version"
 
 module ShortlistImageSearch
+  class Google
+    require 'net/http'
+    attr_accessor :image, :result
+
+    def initialize(image_source)
+      @image = image_source
+      @result = []
+    end
+
+    def search
+      url = uri_processing(@image)
+      @result << {@image=>fetch(url)}
+    end
+
+private
+    def fetch(url, limit = 10)
+      raise ArgumentError, 'too many HTTP redirects' if limit == 0
+      ua  = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36"
+      url = URI.parse(url)
+      req = Net::HTTP::Get.new(url, { 'User-Agent'=> ua })
+      response = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https') do |http|
+        http.request(req)
+      end
+
+      case response
+      when Net::HTTPRedirection then
+        location = response['location']
+        # warn "redirected to #{location}" uncomment for debugging
+        fetch(location, limit - 1)
+      when Net::HTTPSuccess then
+        found = response.body.match(/Pages that include matching images/) ? true : false
+      else
+        response.body
+      end
+
+    end
+
+    def uri_processing(uri)
+      query       = CGI.escape(uri)
+      search_path = "http://images.google.com/searchbyimage?image_url=#{query}&image_content=&filename="
+    end
+  end
 
   class UrlReader
     attr_accessor :urls
@@ -10,9 +52,10 @@ module ShortlistImageSearch
       @urls = []
     end
 
+
     def load(options={})
       File.open(@path).each do |line|
-        @urls << { line => 0 }
+        @urls << {line=>0}
       end
     end
   end
